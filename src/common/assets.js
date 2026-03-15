64a80a3129c703d05712f17d0d640c8155ca337a
scottwestover <scottwestover2006@gmail.com>
2026-03-14 22:00:24 -0400

    initial project setup
    added required project assets
    added project task list
    
    Signed-off-by: scottwestover <scottwestover2006@gmail.com>

--- src/common/assets.js
+++ src/common/assets.js
@@ -1,10 +1,24 @@
 export const ASSET_KEYS = Object.freeze({
-  LOGO: 'LOGO',
+  BACKGROUND: 'BACKGROUND',
+  OBJECTS: 'OBJECTS',
+  JAR: 'JAR',
 });
 
 export const IMAGE_ASSETS = [
   {
-    assetKey: ASSET_KEYS.LOGO,
-    path: 'assets/images/logo.png',
+    assetKey: ASSET_KEYS.BACKGROUND,
+    path: 'assets/images/background.png',
+  },
+  {
+    assetKey: ASSET_KEYS.JAR,
+    path: 'assets/images/jar.png',
+  },
+];
+
+export const TEXTURE_ATLAS_ASSETS = [
+  {
+    assetKey: ASSET_KEYS.OBJECTS,
+    textureURL: 'assets/images/spritesheet.png',
+    atlasURL: 'assets/images/spritesheet.json',
   },
 ];
