350dec7b673ec804aadcb832a9b091c1e9d5c68d
scottwestover <scottwestover2006@gmail.com>
2026-03-16 16:09:32 -0400

    added more required assets
    Signed-off-by: scottwestover <scottwestover2006@gmail.com>

--- src/common/assets.js
+++ src/common/assets.js
@@ -1,12 +1,17 @@
 export const ASSET_KEYS = Object.freeze({
   BACKGROUND: 'BACKGROUND',
   OBJECTS: 'OBJECTS',
+  JAR: 'JAR',
 });
 
 export const IMAGE_ASSETS = [
   {
     assetKey: ASSET_KEYS.BACKGROUND,
     path: 'assets/images/background.png',
+  },
+  {
+    assetKey: ASSET_KEYS.JAR,
+    path: 'assets/images/jar.png',
   },
 ];
 
